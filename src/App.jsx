e3a30fe76e7e55af12e3a334e78a9479e7cad223
antoinelmariel-ux <antoinel.mariel@gmail.com>
2025-10-26 15:11:04 +0100

    Improve onboarding scroll smoothness

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import {
   normalizeProjectFilterConfig
 } from './utils/projectFilters.js';
 
-const APP_VERSION = 'v1.0.160';
+const APP_VERSION = 'v1.0.161';
 
 const BACK_OFFICE_PASSWORD_HASH = '3c5b8c6aaa89db61910cdfe32f1bdb193d1923146dbd6a7b0634a32ab73ac1af';
 const BACK_OFFICE_PASSWORD_FALLBACK_DIGEST = '86ceec83';
@@ -1276,7 +1276,8 @@ export const App = () => {
           behavior: 'smooth',
           block: 'center',
           inline: 'nearest'
-        }
+        },
+        scrollDuration: 3200
       },
       {
         id: 'showcase-edit-trigger',
