512fa1ac943a541aab66577fe78ad708f8d8639b
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-01-19 23:22:41 +0100

    Add Microsoft Graph feature outline

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ import {
 } from './utils/inspirationConfig.js';
 import { exportInspirationToFile } from './utils/inspirationExport.js';
 
-const APP_VERSION = 'v1.0.246';
+const APP_VERSION = 'v1.0.247';
 
 const resolveShowcaseDisplayMode = (value) => {
   if (value === 'light') {
