24627b4146c187bf4bff4c7ca973a9c5a5033c57
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-01-27 09:35:08 +0100

    Bump app version to v1.0.263

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ import {
 import { exportInspirationToFile } from './utils/inspirationExport.js';
 import { normalizeValidationCommitteeConfig } from './utils/validationCommittee.js';
 
-const APP_VERSION = 'v1.0.262';
+const APP_VERSION = 'v1.0.263';
 
 const resolveShowcaseDisplayMode = (value) => {
   if (value === 'light') {
