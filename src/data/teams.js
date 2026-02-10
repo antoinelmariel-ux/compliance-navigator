da37f7476bff5184538bd4fd3c828d30f25edfc5
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-02-10 13:02:40 +0100

    Add files via upload

--- src/data/teams.js
+++ src/data/teams.js
@@ -1,50 +1,80 @@
 export const initialTeams = [
   {
     "id": "bpp",
-    "name": "Bonnes Pratiques Promotionnelles",
-    "contacts": ["bpp@lfb.com", "bpp-support@lfb.com"],
-    "expertise": "Conformité réglementaire des communications pour éviter tout risque de promotion non adaptée"
+    "name": "Contrôle pub",
+    "contacts": [
+      "laure.dabel@lfb.fr"
+    ],
+    "expertise": "Le contrôle pub permet d’anticiper les exigences réglementaires en matière de communication autour de nos produits. Une sollicitation avant toute création de contenu par une agence évitera des retours coûteux et inutiles."
   },
   {
     "id": "it",
-    "name": "IT & Sécurité",
-    "contacts": ["it-security@lfb.com", "support-it@lfb.com"],
-    "expertise": "Évaluer la sécurité et la robustesse technique du projet"
+    "name": "Affaires Publiques",
+    "contacts": [
+      "elzaabi@lfb.fr",
+      "queres@lfb.fr"
+    ],
+    "expertise": "Le pôle Affaires publiques accompagne les projets à destination des patients et des associations de patients afin de garantir des interactions responsables, transparentes et conformes aux codes et référentiels en vigueur, en France comme à l’international."
   },
   {
     "id": "legal",
-    "name": "Juridique",
-    "contacts": ["legal@lfb.com", "juridique-conseil@lfb.com"],
-    "expertise": "Sécuriser les aspects contractuels et la conformité réglementaire des documents et relations"
+    "name": "Juridique France",
+    "contacts": [
+      "hantzbergk@lfb.fr",
+      "vanessa.dealmeida@lfb.fr"
+    ],
+    "expertise": "Le pôle juridique France accompagne les équipes dans la conception, la structuration et la sécurisation juridique des projets en France et des projets IT.\nSon rôle est d’anticiper les risques juridiques et de protéger le LFB contre une mauvaise exécution du contrat par le partenaire."
   },
   {
     "id": "privacy",
-    "name": "Privacy & RGPD",
-    "contacts": ["privacy@lfb.com", "dpo@lfb.com"],
-    "expertise": "Protection des données personnelles et conformité RGPD"
+    "name": "Juridique International",
+    "contacts": [
+      "juliette.woimant@lfb.fr",
+      "chantelouplaurine@lfb.fr",
+      "beremel@lfb.fr"
+    ],
+    "expertise": "Le pôle PIA accompagne les équipes dans la conception, la structuration et la sécurisation juridique des projets à l’international.\nSon rôle est d’anticiper les risques juridiques et de protéger le LFB contre une mauvaise exécution du contrat par le partenaire."
   },
   {
     "id": "procurement",
-    "name": "Achat",
-    "contacts": ["achat@company.com", "procurement@company.com"],
-    "expertise": "Garantir que les prestataires respectent les procédures éthiques et financières"
+    "name": "DPO",
+    "contacts": [
+      "c.spiesserlacombe@lfb.fr"
+    ],
+    "expertise": "Le Délégué à la Protection des Données (DPO) vous accompagne dans la conception de projets respectueux de la vie privée, en garantissant la conformité au RGPD et aux réglementations locales applicables. "
   },
   {
     "id": "pv",
     "name": "Pharmacovigilance",
-    "contacts": ["pv@company.com", "pharmacovigilance@company.com"],
-    "expertise": "Garantir la remontée des événements indésirables liés à un produit"
+    "contacts": [
+      "pharmacovigilance@lfb.fr"
+    ],
+    "expertise": "Le département Pharmacovigilance a pour rôle clé de permettre de sécuriser les canaux de communication par lesquels pourraient être remontés - même de manière non prévue - des cas de pharmacovigilance afin de respecter nos obligations en la matière."
   },
   {
     "id": "com",
-    "name": "Communication",
-    "contacts": ["communication@company.com", "brand@company.com"],
-    "expertise": "S’assurer du respect des guidelines visuelles et de la réputation de l’entreprise"
+    "name": "Communication externe",
+    "contacts": [
+      "renaudate@lfb.fr"
+    ],
+    "expertise": "La communication externe a pour mission de protéger, valoriser et rendre cohérente l’image du Groupe, en France comme à l’international avec le déploiement de messages factuels et alignés avec notre identité de marque, nos valeurs et notre stratégie."
   },
   {
     "id": "Ethics",
     "name": "Ethique & Compliance",
-    "contacts": ["ethique@company.com", "compliance@company.com"],
-    "expertise": "Garantir la conformité globale du projet à la réglementation interne et externe (dont Loi d'Encadrement des Avantages)"
+    "contacts": [
+      "antoine.lassauge@lfb.fr"
+    ],
+    "expertise": "Le pôle Éthique & Compliance accompagne les équipes dans la conception et la mise en œuvre de projets conformes aux réglementations applicables aux interactions entre les laboratoires pharmaceutiques, les professionnels de santé, les patients et les autres parties prenantes."
+  },
+  {
+    "id": "team1",
+    "name": "PI",
+    "contacts": [
+      "audrey.dupuy@lfb.fr",
+      "letexierl@lfb.fr",
+      "vauvrecys@lfb.fr"
+    ],
+    "expertise": "L’équipe Propriété intellectuelle a pour mission de protéger, valoriser et sécuriser les actifs immatériels du Groupe, tout en veillant à ce que les projets ne portent pas atteinte aux droits de propriété intellectuelle des tiers."
   }
 ];
