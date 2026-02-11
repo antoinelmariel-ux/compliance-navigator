14e063e3d74f1d6876666c83e567c3b4edbe24ee
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-02-11 13:51:12 +0100

    Activer la sauvegarde locale et passer la version footer en v1.0.284

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,6 @@
 export const STORAGE_KEY = 'complianceNavigatorState';
-// Désactivation temporaire de la persistance locale.
-const ENABLE_PERSISTENCE = false;
+// Persistance locale activée pour conserver l'état entre les sessions.
+const ENABLE_PERSISTENCE = true;
 
 export const loadPersistedState = () => {
   if (
