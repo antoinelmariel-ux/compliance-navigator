6c788fe074462fb75ee55a10508bb74674b2761a
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-03-17 10:26:43 +0100

    Fix local file bootstrap path resolution and bump app version

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ import { inspirationDataProvider } from './utils/inspirationDataProvider.js';
 import { createAutosaveQueue } from './utils/autosaveQueue.js';
 import { reinitializeSharePointConfiguration } from './utils/sharePointSetup.js';
 
-const APP_VERSION = 'v1.0.366';
+const APP_VERSION = 'v1.0.367';
 
 class AdminBackOfficeErrorBoundary extends React.Component {
   constructor(props) {
